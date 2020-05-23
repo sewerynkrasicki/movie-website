7cc314fbc141e5780176463301b894554335a10d
Seweryn <seweryn.krasicki@protonmail.com>
2020-05-24 00:23:25 +0200

    Base template, index site and some static files

---
+++ movie/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('/movie', views.movie_list, name="movie_list"),
+]
--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def movie_list(request):
+    return render(request, 'movie/movie_list.html', {})
+
+def index(request):
+    return render(request, 'movie/index.html', {})
+
